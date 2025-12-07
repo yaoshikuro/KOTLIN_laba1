9e6c70534f5fa772fbd7fff7d2210ff1037a9836
salmanishee <yukivoron@gmail.com>
2025-12-07 21:22:06 +0300

    Add files via upload

---
+++ src/app.kt
@@ -0,0 +1,163 @@
+const val  PI = 3.1415
+fun main() {
+//    println("Hello world")
+//    println("Today is sunny!")
+//    println("тут очень большой текст который у меня не помещается")
+//
+//    val city = "Москва"
+//    var temperature = 20
+//    temperature += 5
+//    println(PI)
+//    val count: Int =2
+//    println (count)
+//    val game = "Cyberpunk"
+//    val year = 2012
+//    val message = "$game - пользовательская карта для варкрафт 3 разрабатываемая с $year"
+//
+//    zadanie
+//    val name = "Salman" //неиз. переменная строки
+//    val age = 18 //неиз. числовая переменная
+//    println("hello") //вывод строки
+//    println("my name is $name") //вывод переменной имени
+//    println("my age is $age") //вывод переменной возраста
+//    val a =10 //неиз. числовая переменная
+//    val b = 15 //неиз. числовая переменная
+//    val c = a + b + 8 //неиз. числовая переменная (сумма других перемен и доп. числа)
+//    val str = c.toString() //превращение числовой переменно в строковую
+//    println(str) //вывод превращенной переменны
+//    val numlist = arrayOf(1,2,3) //создание списка
+//    var x = 0 //изм. числовая переменная
+//    while (x <3) { //условие цикла
+//        println ("Item $x is ${numlist}") //вывод цикла по номеру
+//        x = x + 1 //с каждым цкилом к переменной +1
+//
+//    val number: Double =12.7
+//    val integerNumber: Int = number.toInt()
+//    println(integerNumber)
+//
+//    val a: Int = 7
+//    val b: Double = 3.5
+//    val c: Int = b.toInt()
+//    println(a+c)
+//    println(a/c)
+//    var x: Double = 65.2
+//    var isPunk: Int = true
+//    var message = "hello"
+//    isPunk = 10
+//    var y: String = "15"
+//    y = y + "helloo"
+
+//    val unread = 5
+//    val read = 100
+//    println("У вас ${unread + read} сообщений")
+////
+//    val a = 10
+//    val b = 3
+//    println("Сумма: ${a+b}")
+//    println("Разность: ${a-b}")
+//    println("Произведение: ${a*b}")
+//    println("Частное: ${a/b}")
+//    println("остаток: ${a%b}")
+//
+//    println("Ввудите число А")
+//    val a = readln().toInt()
+//    println("Ввудите число В")
+//    val b = readln().toInt()
+//    println("Сумма: ${a+b}")
+//    println("Разность: ${a-b}")
+//    println("Произведение: ${a*b}")
+//    println("Частное: ${a/b}")
+//    println("остаток: ${a%b}")
+
+//    var count: Int = 10
+//    count = count + 1
+//    println(count)
+//    count++
+//    println(count)
+//    count--
+//    println(count)
+//
+//    val a = 5
+//    val b = 2
+//    println(a / b)
+//    println(a % b)
+//
+//    val totalsec = readln().toInt()
+//    val hours = totalsec / 3600
+//    val minut = totalsec % 3600 / 60
+//    val sec = totalsec % 3600 % 60
+//    println("$hours ч, $minut мин, $sec сек")
+
+//    val far = readln().toDouble()
+//    val cels = (far - 32)*(5/9)
+//    println("$far F = $cels C")
+
+
+    //zadanie 1
+//    val game = "cyberpunk 2077"
+//    val num = 8
+//    val bukva = "S"
+//    val PI = 3.14
+//    println("любимая игра: $game, любимое число: $num, любимая буква: $bukva, число пи: $PI")
+
+    //zadanie 2
+//    val a = 8
+//    a = 10
+//    print("helli"
+//    val q: Int = "ll"
+
+//    val pi: Double = 3.14
+//    val r: Int = 5
+//    println(pi*r*r)
+//
+//    println("введите ваше имя: ")
+//    val name = readln()
+//    println("введите ваш год  рождение: ")
+//    val god = readln().toInt()
+//    println("$name, вам ${2025 - god} лет")
+
+//
+//    println("Ввудите число А")
+//    val a = readln().toInt()
+//    println("Ввудите число В")
+//    val b = readln().toInt()
+//    println("$a + $b = ${a + b}")
+//    println("$a - $b = ${a - b}")
+//    println("$a * $b = ${a * b}")
+
+//    println("введите число: ")
+//    val num = readln().toInt()
+//    val a = num/100
+//    val b = num/10%10
+//    val c = num%100%10
+//    println(a+b+c)
+
+//    println("введите разделитель: ")
+//    val zn = readln()
+//    println("введите что нибудь: ")
+//    val a = readln()
+//    println("введите что нибудь: ")
+//    val b = readln()
+//    println("введите что нибудь: ")
+//    val q = readln()
+//    println("$a$zn$b$zn$q")
+
+//    println("введите число")
+//    var num = readln().toInt()
+//    var x = 0
+//    while (x < 3) {
+//        println(num)
+//        num+=1
+//        x+=1
+//    }
+//    println("введите число: ")
+//    val num = readln().toInt()
+//    val a = num/100
+//    val b = num/10%10
+//    val c = num%100%10
+
+//    println("введите число: ")
+//    val num = readln().toInt()
+//    val sot = num/100%10
+//    print(sot)
+}
---
+++ src/person.kt
@@ -0,0 +1,15 @@
+fun main() {
+//    println("Введите имя пользователя: ")
+//    val name = readln()
+//    println("Введите ваш возраст: ")
+//    val age = readln()
+//    println("Введите ваш пол: ")
+//    val pol = readln()
+//    println("Ваше имя - $name")
+//    println("Ваше возраст - $age")
+//    println("Ваше пол - $pol")
+//    println("Ваше имя: $name, Ваш возраст: $age, Ваш пол: $pol")
+//    val ageint = age.toInt() + 5
+//    print("Ваш возраст через 5 лет: $ageint")
+
+}
